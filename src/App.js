2a9133d8cb41375753c45449c62ff9a8c08aee96
IvarTandberg <122887791+IvarTandberg@users.noreply.github.com>
2023-03-27 12:19:37 +0200

    start prosjekt, react, sass, bootstrap

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import './css/main.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
