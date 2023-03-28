bbc42563fcef40d7f3698b4cf228317d981cc6b9
IvarTandberg <122887791+IvarTandberg@users.noreply.github.com>
2023-03-28 16:24:50 +0200

    api

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import './css/main.css';
+import Main from './components/Main'
 
 
 function App() {
   return (
     <div className="App">
-      
+      <Main />
     </div>
   );
 }
